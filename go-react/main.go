ea27bca0b8cba703da03f0433f43cb09f443bcbc
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2021-04-20 17:06:59 -0700

    test

--- go-react/main.go
+++ go-react/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type menuItem struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Name       *string            `json:"name" validate:"required,min=2,max=100"`
+	Price      *float64           `json:"price" validate:"required"`
+	Food_image *string            `json:"food_image" validate:"required"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
+	Food_id    string             `json:"food_id"`
+}
+
 func main() {
 
 }
